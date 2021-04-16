0ab4c3235387865b105b9b06655c3ea6457c75d0
Lidne <play.gt.play@yandex.ru>
2021-04-16 17:11:52 +0300

    First commit. Currently not working

---
+++ data/games.py
@@ -0,0 +1,19 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class Games(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'games'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    developers = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    release_date = sqlalchemy.Column(sqlalchemy.Date, nullable=True)
+    is_selling = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
